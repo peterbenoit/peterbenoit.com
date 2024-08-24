76480284a192af7fa4c8fbba1e5db7f2f50f6a00
Pete Benoit <peterbenoit@gmail.com>
2024-08-24 19:14:42 -0400

    first pages

--- portfolio/src/components/layout.js
+++ portfolio/src/components/layout.js
@@ -1,51 +1,18 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.com/docs/how-to/querying-data/use-static-query/
- */
+import React from 'react'
+import { Link } from 'gatsby'
 
-import * as React from "react"
-import { useStaticQuery, graphql } from "gatsby"
-
-import Header from "./header"
-import "./layout.css"
-
-const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
-
-  return (
-    <>
-      <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: `var(--size-content)`,
-          padding: `var(--size-gutter)`,
-        }}
-      >
-        <main>{children}</main>
-        <footer
-          style={{
-            marginTop: `var(--space-5)`,
-            fontSize: `var(--font-sm)`,
-          }}
-        >
-          © {new Date().getFullYear()} &middot; Built with
-          {` `}
-          <a href="https://www.gatsbyjs.com">Gatsby</a>
-        </footer>
-      </div>
-    </>
-  )
-}
+const Layout = ({ children }) => (
+  <div>
+    <header className="bg-gray-800 p-4 text-white">
+      <nav className="container mx-auto">
+        <Link to="/" className="mr-4">Home</Link>
+        <Link to="/about" className="mr-4">About</Link>
+        <Link to="/projects" className="mr-4">Projects</Link>
+        <Link to="/contact">Contact</Link>
+      </nav>
+    </header>
+    <main>{children}</main>
+  </div>
+)
 
 export default Layout
