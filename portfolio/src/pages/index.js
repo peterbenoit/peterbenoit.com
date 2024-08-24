29d7f780f24b750c11a596d19f4e14848b274226
Pete Benoit <peterbenoit@gmail.com>
2024-08-24 19:28:45 -0400

    setting up tailwind

--- portfolio/src/pages/index.js
+++ portfolio/src/pages/index.js
@@ -1,13 +1,11 @@
 import React from 'react'
-import { Link } from 'gatsby'
 import Layout from '../components/layout'
 
 const IndexPage = () => (
-  <main className="container mx-auto p-4">
-    <h1 className="text-4xl font-bold">Welcome to My Portfolio</h1>
-    <p className="mt-2">I’m Peter Benoit, a web developer and software engineer.</p>
-    <Link to="/about" className="text-blue-500">Learn more about me</Link>
-  </main>
+  <Layout>
+    <h1 className="text-4xl font-bold text-center">Welcome to My Portfolio</h1>
+    <p className="mt-4 text-center">I’m Peter Benoit, a web developer and software engineer.</p>
+  </Layout>
 )
 
 export default IndexPage
