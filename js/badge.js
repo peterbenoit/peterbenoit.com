7689cef90a96d508150ff433be4d81ab8ba7f8c7
Pete Benoit <peterbenoit@gmail.com>
2024-10-02 18:57:21 -0400

    Update badge.js

--- js/badge.js
+++ js/badge.js
@@ -9,22 +9,22 @@
     footer.style.alignItems = 'center';
     footer.style.gap = '5px';
     footer.style.fontFamily = 'Arial, sans-serif';
-    footer.style.color = '#2b2b2b';
+    footer.style.color = '#fff';
     footer.style.fontSize = '14px';
-    footer.style.background = 'rgba(0, 0, 0, 0.1)';
+    footer.style.background = 'rgba(0, 0, 0, 0.5)';
     footer.style.padding = '10px';
-    
+
     const style = document.createElement('style');
     style.innerHTML = `
 		#footer-${randomId} a {
-			color: #2b2b2b;
+			color: #fff;
 			text-decoration: none;
 			font-weight: bold;
 			transition: filter .3s ease;
 		}
 		#footer-${randomId} a svg {
 			transition: filter .3s ease;
-			stroke: #2b2b2b;
+			stroke: #fff;
 		}
 		#footer-${randomId} a:hover {
 			color: #ff00ff;
@@ -45,7 +45,7 @@
                 opacity: 1;
                 transform: translateX(0);
             }
-        }		
+        }
 		@keyframes rainbow {
 			0% { filter: hue-rotate(300deg) saturate(3) brightness(1.2); }
 			10% { filter: hue-rotate(290deg) saturate(3) brightness(1.2); }
@@ -58,7 +58,7 @@
 			80% { filter: hue-rotate(280deg) saturate(3) brightness(1.2); }
 			90% { filter: hue-rotate(290deg) saturate(3) brightness(1.2); }
 			100% { filter: hue-rotate(300deg) saturate(3) brightness(1.2); }
-		}    
+		}
     `;
     document.head.appendChild(style);
 
@@ -83,13 +83,13 @@
                 <path d="M7 17V13.5V10" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
                 <path d="M11 17V13.75M11 10V13.75M11 13.75C11 10 17 10 17 13.75V17" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
                 <path d="M7 7.01L7.01 6.99889" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
-            </svg>` 
+            </svg>`
         },
         { href: 'https://github.com/peterbenoit', svg: `
             <svg width="24px" height="24px" stroke-width="1.5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M16 22.0268V19.1568C16.0375 18.68 15.9731 18.2006 15.811 17.7506C15.6489 17.3006 15.3929 16.8902 15.06 16.5468C18.2 16.1968 21.5 15.0068 21.5 9.54679C21.4997 8.15062 20.9627 6.80799 20 5.79679C20.4558 4.5753 20.4236 3.22514 19.91 2.02679C19.91 2.02679 18.73 1.67679 16 3.50679C13.708 2.88561 11.292 2.88561 8.99999 3.50679C6.26999 1.67679 5.08999 2.02679 5.08999 2.02679C4.57636 3.22514 4.54413 4.5753 4.99999 5.79679C4.03011 6.81549 3.49251 8.17026 3.49999 9.57679C3.49999 14.9968 6.79998 16.1868 9.93998 16.5768C9.61098 16.9168 9.35725 17.3222 9.19529 17.7667C9.03334 18.2112 8.96679 18.6849 8.99999 19.1568V22.0268" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
                 <path d="M9 20.0267C6 20.9999 3.5 20.0267 2 17.0267" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
-            </svg>` 
+            </svg>`
         },
         { href: 'https://codepen.io/peterbenoit', svg: `
             <svg width="24px" height="24px" stroke-width="1.5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -99,7 +99,7 @@
                 <path d="M12 3V9" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
                 <path d="M12 15L3 9L12 3L21 9L12 15Z" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
                 <path d="M12 21L3 15L12 9L21 15L12 21Z" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
-            </svg>` 
+            </svg>`
         },
     ];
 
