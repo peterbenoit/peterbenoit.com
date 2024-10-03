7dc64b85f0a9baa792dd4fbe2126b2afff64f7e3
Pete Benoit <peterbenoit@gmail.com>
2024-10-03 12:40:09 -0400

    exclusions

--- js/autoaxe.js
+++ js/autoaxe.js
@@ -1,26 +1,30 @@
 (function () {
-	const randomId = Math.random().toString(36).slice(2);
+    const randomId = Math.random().toString(36).slice(2);
     function gAT(t) {
         let e = `<table class="styled-table-${randomId}">`;
-        e += "<thead><tr><th>Issue</th><th>Description</th><th>Impact</th><th>Element</th></tr></thead>";
-        e += "<tbody>";
-        t.forEach(t => {
+        e +=
+            '<thead><tr><th>Issue</th><th>Description</th><th>Impact</th><th>Element</th></tr></thead>';
+        e += '<tbody>';
+        t.forEach((t) => {
             let l = cLFE(t.nodes[0].target);
             e += `<tr><td>${t.id}</td><td>${t.description}</td><td>${t.impact}</td><td>${l}</td></tr>`;
         });
-        e += "</tbody>";
-        e += "</table>";
+        e += '</tbody>';
+        e += '</table>';
         return e;
     }
 
-	function cLFE(target) {
-		const targetString = Array.isArray(target) ? target[0] : target;
-		const cleanTarget = targetString.replace(/\s/g, '').replace(/"/g, '&quot;').replace(/^#/, '');
-		return `<a href="#${cleanTarget}">${targetString}</a>`;
-	}
+    function cLFE(target) {
+        const targetString = Array.isArray(target) ? target[0] : target;
+        const cleanTarget = targetString
+            .replace(/\s/g, '')
+            .replace(/"/g, '&quot;')
+            .replace(/^#/, '');
+        return `<a href="#${cleanTarget}">${targetString}</a>`;
+    }
 
     function createAccessibilityIcon() {
-        const icon = document.createElement("div");
+        const icon = document.createElement('div');
         icon.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="currentColor"><path d="M0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256zm161.5-86.1c-12.2-5.2-26.3 .4-31.5 12.6s.4 26.3 12.6 31.5l11.9 5.1c17.3 7.4 35.2 12.9 53.6 16.3l0 50.1c0 4.3-.7 8.6-2.1 12.6l-28.7 86.1c-4.2 12.6 2.6 26.2 15.2 30.4s26.2-2.6 30.4-15.2l24.4-73.2c1.3-3.8 4.8-6.4 8.8-6.4s7.6 2.6 8.8 6.4l24.4 73.2c4.2 12.6 17.8 19.4 30.4 15.2s19.4-17.8 15.2-30.4l-28.7-86.1c-1.4-4.1-2.1-8.3-2.1-12.6l0-50.1c18.4-3.5 36.3-8.9 53.6-16.3l11.9-5.1c12.2-5.2 17.8-19.3 12.6-31.5s-19.3-17.8-31.5-12.6L338.7 175c-26.1 11.2-54.2 17-82.7 17s-56.5-5.8-82.7-17l-11.9-5.1zM256 160a40 40 0 1 0 0-80 40 40 0 1 0 0 80z"/></svg>`;
         icon.style.position = 'fixed';
         icon.style.top = '20px';
@@ -93,31 +97,42 @@
         document.head.appendChild(axeCDN);
 
         axeCDN.onload = function () {
-            axe.run({ rules: { "landmark-one-main": { enabled: !1 }, "page-has-heading-one": { enabled: !1 }, region: { enabled: !1 } } }, function (t, e) {
-                const icon = createAccessibilityIcon();
-                const panel = createFloatingPanel();
+            axe.run(
+                {
+                    exclude: [[`styled-table-${randomId}`]],
+                    rules: {
+                        'landmark-one-main': { enabled: !1 },
+                        'page-has-heading-one': { enabled: !1 },
+                        region: { enabled: !1 },
+                    },
+                },
+                function (t, e) {
+                    const icon = createAccessibilityIcon();
+                    const panel = createFloatingPanel();
 
-                console.log(t, e);
-                if (e.violations.length) {
-                    let tableContent = gAT(e.violations);
-                    panel.innerHTML = tableContent;
-                    icon.style.display = 'block';
+                    if (e.violations.length) {
+                        let tableContent = gAT(e.violations);
+                        panel.innerHTML = tableContent;
+                        icon.style.display = 'block';
 
-                    icon.addEventListener('click', function () {
-                        panel.style.display = panel.style.display === 'none' ? 'block' : 'none';
-                    });
-
-                    document.querySelectorAll("table a").forEach(link => {
-                        link.addEventListener("click", () => {
-                            document.querySelectorAll(link.hash.split("#").pop())[0].style = "border:1px solid red;background:yellow";
+                        icon.addEventListener('click', function () {
+                            panel.style.display = panel.style.display === 'none' ? 'block' : 'none';
                         });
-                    });
-                } else {
-                    console.log("No accessibility violations found.");
+
+                        document.querySelectorAll('table a').forEach((link) => {
+                            link.addEventListener('click', () => {
+                                console.log(link.hash);
+                                document.querySelectorAll(link.hash.split('#').pop())[0].style =
+                                    'border:1px solid red;background:yellow';
+                            });
+                        });
+                    } else {
+                        console.log('No accessibility violations found.');
+                    }
                 }
-            });
+            );
         };
     }
 
-    document.addEventListener("DOMContentLoaded", runAxeAnalysis);
+    document.addEventListener('DOMContentLoaded', runAxeAnalysis);
 })();
